4052c65a25b9be94af96216f81308597a785fad7
ManuelCM21 <>
2021-04-26 14:01:06 +0000

    Creando kotlin, dart y scala

---
+++ AreaTriangulo.rb
@@ -0,0 +1,10 @@
+#definicion de variables u otros
+puts("Ejercicio 01: Area Triangulo")
+#Datos de Entrada
+puts "Ingrese base: "
+b = gets.to_i
+puts "Ingrese altura: "
+h = gets.to_i
+area = b*h/2
+#Datos de Salida
+puts "El area del trianuglo es: #{area}"
