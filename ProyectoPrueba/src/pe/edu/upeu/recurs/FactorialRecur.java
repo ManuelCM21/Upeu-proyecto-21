350be16616376e134c069d97d594a9b70565ddde
ManuelCM21 <manuelchuncamamani@gmail.com>
2021-06-02 10:26:03 -0500

    Proyecto Prueba con subprogramas

---
+++ ProyectoPrueba/src/pe/edu/upeu/recurs/FactorialRecur.java
@@ -0,0 +1,26 @@
+package pe.edu.upeu.recurs;
+
+import java.math.BigInteger;
+
+public class FactorialRecur {
+    
+    public long factorial(int n) {
+        long resultF=1;
+        if (n>1) {
+            for (int i=1; i<=n; i++) {
+                resultF*=i;
+            }
+        }
+        return resultF;
+    }
+
+    public BigInteger recursividad(int n) {
+        BigInteger resultF=new BigInteger("1");
+        if (n>1) {
+            for(int i=1; i<=n; i++){
+            resultF=resultF.multiply(new BigInteger(String.valueOf(i)));
+        }
+    }
+    return resultF;
+}
+}
